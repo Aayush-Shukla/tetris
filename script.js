20c8313c543cd6e392aa330bbefdb9949dff9d20
Ayush Shukla <34869001+Aayush-Shukla@users.noreply.github.com>
2020-09-17 00:24:52 +0530

    feat: movable piece

--- script.js
+++ script.js
@@ -14,23 +14,29 @@ var mapcol=10;
 
 
 var screen=[
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,1,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
+    
 
 ]
+
+var piece=[[0,0,0],
+            [1,1,1],
+            [0,1,0]
+                    ]
 
 
 
@@ -52,13 +58,50 @@ var drawImage =function f() {
 
             
             
-                ctx.drawImage(tileset, (tilesize * tileX), (tileY * tilesize), tilesize, tilesize, (c * tilesize ), (r * tilesize ), tilesize , tilesize )
+                ctx.drawImage(tileset, (0), (0), tilesize, tilesize, (c * tilesize ), (r * tilesize ), tilesize , tilesize )
             
         //
+
         }
     }
+    drawMatrix(piece,player.pos)
     }
 
    
 
+
+    function drawMatrix(matrix, offset) {
+        for (var r=0;r<matrix.length;r++){
+            for (var c=0;c<matrix[r].length;c++) {
+                // console.log(map[10*r+c])
+                // var tile = screen[10*r+c]
+    
+                // var tileX = tile % tilecol
+                // var tileY = Math.floor(tile / tilecol)
+                // console.log("hi")
+                
+    
+                
+                    if(matrix[r][c]!==0){
+                    ctx.drawImage(tileset, tilesize, (0), tilesize, tilesize, (c * tilesize )+offset.x*tilesize, (r * tilesize )+offset.y*tilesize, tilesize , tilesize )
+                    }
+            }
+        }
+    }
+    
+    const player = {
+        pos: {x: 5, y: 5},
+        matrix: piece,
+    };
+
+
+
+setInterval(update,1000)
+
+function update(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    player.pos.y++
+    drawImage()
+    drawMatrix(piece,player.pos)
+}
 tileset.onload = drawImage
